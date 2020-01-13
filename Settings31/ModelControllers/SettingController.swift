2fbb138325d891af6a3c70c99b5299d07208ff72
jon <jdcorn@live.com>
2020-01-13 11:36:32 -0700

    added controllers

--- Settings31/ModelControllers/SettingController.swift
+++ Settings31/ModelControllers/SettingController.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class SettingController {
+    
+    // Properties
+    static let shared = SettingController()
+    let settings: [Setting] = {
+        let iTunes = Setting(name: "iTunes", icon: #imageLiteral(resourceName: "iTunes"), isOn: false)
+        let AppStore = Setting(name: "AppStore", icon: #imageLiteral(resourceName: "AppStore"), isOn: false)
+        let Books = Setting(name: "Books", icon: #imageLiteral(resourceName: "Books"), isOn: false)
+        
+        return [iTunes, AppStore, Books]
+    }()
+    
+    // Functions
+    func toggleIsOn(for setting: Setting) {
+        setting.isOn.toggle()
+    }
+}
---
+++ Settings31/ViewControllers/SettingTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SettingTableViewCell.swift
+//  Settings31
+//
+//  Created by Jon Corn on 1/13/20.
+//  Copyright © 2020 jdcorn. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var settingSwitch: UISwitch!
+    
+    // Actions
+    
+    
+}
